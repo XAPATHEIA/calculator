4ee1cc3438d139d72311950041eedcae1516538c
xapatheia <54541033+XAPATHEIA@users.noreply.github.com>
2023-10-15 15:42:23 +0100

    implement basic operator functions

---
+++ script.js
@@ -0,0 +1,30 @@
+// Defining math operator functions 
+
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+function divide(a, b) {
+    if (a === 0 || b === 0) {
+        console.log("Cannot divide by zero");
+    } else {
+        return a / b;
+    }
+}
+
+function operate(a, operator, b) {
+    if (operator == '+') {
+        add(a, b);
+    } else if (operator == '-') {
+        subtract(a, b);
+    } else if (operator == '*') {
+        multiply(a, b);
+    } else if (operator == '/') {
+        divide(a, b);
+    } else {
+        console.log("No operator found");
+    }
+}
+
