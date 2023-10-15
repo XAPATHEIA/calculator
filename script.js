d4bd0e5f5ed7b9cc4e8e36694485c6ea312e26b0
xapatheia <54541033+XAPATHEIA@users.noreply.github.com>
2023-10-15 17:36:25 +0100

    improve display capabilities

--- script.js
+++ script.js
@@ -40,11 +40,34 @@ calculatorButtons.forEach((button) => {
 // Variable to hold display element
 displayElement = document.getElementById('calculator-output');
 
+let operators = ['%', '/', '+', '-', '×'];
+
+function checkForExistence(operator, array) {
+    for (i = 0; i < array.length; i++) {
+        if (array[i] === operator) {
+            return true;
+        }
+    }
+    return false;
+}
+
 function updateDisplay(event) {
     const button = event.target;
+    // Will clear display for now
     if (button.value == 'AC' || button.value == 'CE') {
         displayElement.innerText = '';
+    } else if (button.value == '=') {
+        calculate();
+    // If an operator has been pressed, then will add spacing to the display
+    } else if (checkForExistence(button.value, operators)) {      
+        displayElement.innerText += ' ' + button.value;
+    // if an operator is at the end of the current display value, will add spacing to the display
+    } else if (checkForExistence(displayElement.innerText.slice(-1), operators)) {
+        displayElement.innerText += ' ' + button.value;
     } else {
         displayElement.innerText += button.value;
     }
 }
+
+
+
