f888a35091de07b51b6e610403ccd6de663d63bb
xapatheia <54541033+XAPATHEIA@users.noreply.github.com>
2023-10-15 19:19:43 +0100

    add operational capability for the handling of one operator, no operator precedence implemented yet

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const multiply = (a, b) => a * b;
 function divide(a, b) {
     if (a === 0 || b === 0) {
         console.log("Cannot divide by zero");
+        return 'Error';
     } else {
         return a / b;
     }
@@ -28,6 +29,16 @@ function operate(a, operator, b) {
     }
 }
 
+const operations = {
+    '%': 'modulo',
+    '/': divide,
+    '×': multiply,
+    '+': add,
+    '-': subtract
+}
+
+
+
 // Event listener on click for all buttons
 let calculatorContainer = document.getElementById('calculator-container');
 let calculatorButtonContainer = document.getElementById('calculator-buttons');
@@ -40,7 +51,7 @@ calculatorButtons.forEach((button) => {
 // Variable to hold display element
 displayElement = document.getElementById('calculator-output');
 
-let operators = ['%', '/', '+', '-', '×'];
+let operators = ['%', '/', '×', '+', '-']
 
 function checkForOperator(operator) {
     for (i = 0; i < operators.length; i++) {
@@ -71,11 +82,37 @@ function calculate(finalCalcString) {
         }
     }
 
-    console.log('its fine');
+    let numberA = '';
+    let numberB = '';
+    let numberACompleted = false;
+    let numberBCompleted = false;
+    let operator = '';
 
+    for (let index = 0; index < finalCalcArray.length; index++) {
+        if (!(checkForOperator(finalCalcArray[index])) && !(numberACompleted)) {
+            numberA += finalCalcArray[index];
+        } else if (!(checkForOperator(finalCalcArray[index])) && !(numberBCompleted)) {
+            numberB += finalCalcArray[index];
+        } else if (checkForOperator(finalCalcArray[index])) {
+            numberACompleted = true;
+            operator = finalCalcArray[index];
+        } else {
+            finalCalcArray.splice(0, index-1);
+            numberBCompleted = true;
+        }
+    }
+
+    console.log(finalCalcArray)
+    console.log(numberA);
+    console.log(numberB);
+    console.log(operator);
+
+    runningCalculation = operations[operator](numberA, numberB);
+    
+    console.log(runningCalculation);
 }
 
-
+let runningCalculation = 0;
 
 function updateDisplay(event) {
     const button = event.target;
