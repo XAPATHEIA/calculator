50ea173379f8142c77711ccf443f64cb1eedc811
xapatheia <54541033+XAPATHEIA@users.noreply.github.com>
2023-10-21 16:13:38 +0100

    add working precedence

--- script.js
+++ script.js
@@ -1,35 +1,22 @@
 // Defining math operator functions 
 
-const add = (a, b) => a + b;
+const add = (a, b) => parseInt(a) + parseInt(b);
 
-const subtract = (a, b) => a - b;
+const subtract = (a, b) => parseInt(a) - parseInt(b);
 
-const multiply = (a, b) => a * b;
+const multiply = (a, b) => parseInt(a) * parseInt(b);
 
 function divide(a, b) {
-    if (a === 0 || b === 0) {
+    if (parseInt(a) === 0 || parseInt(b) === 0) {
         console.log("Cannot divide by zero");
         return 'Error';
     } else {
-        return a / b;
+        return parseInt(a) / parseInt(b);
     }
 }
 
-const modulo = (a, b) => a % b;
+const modulo = (a, b) => parseInt(a) % parseInt(b);
 
-function operate(a, operator, b) {
-    if (operator == '+') {
-        add(a, b);
-    } else if (operator == '-') {
-        subtract(a, b);
-    } else if (operator == '*') {
-        multiply(a, b);
-    } else if (operator == '/') {
-        divide(a, b);
-    } else {
-        console.log("No operator found");
-    }
-}
 
 const operations = {
     '%': modulo,
@@ -38,7 +25,6 @@ const operations = {
     '+': add,
     '-': subtract
 }
-
 
 
 // Event listener on click for all buttons
@@ -128,6 +114,7 @@ function calculate(finalCalcString) {
         }   
         var numberIndex = finalCalcArray.indexOf(operator) - 1;
         var endOfnumberAIndex = numberIndex;
+        var startOfnumberAIndex = 0;
         while (!cfo(finalCalcArray[numberIndex]) && numberIndex >= 0) {
             numberA += finalCalcArray[numberIndex];
             if (cfo(finalCalcArray[numberIndex-1]) || numberIndex-1 < 0) {
@@ -137,24 +124,20 @@ function calculate(finalCalcString) {
         }
         numberA = (reverse(numberA)).join('');
 
-        console.log(numberA);
-        console.log(numberB);
-        console.log(finalCalcArray);
-        // TODO: Start calculating here, index into original array and create new array so as to not constantly shift elements around
-        console.log("Start of numberB is " + startOfnumberBIndex);
-        console.log("End of numberB is " + endOfnumberBIndex);
-        console.log("Start of numberA is " + startOfnumberAIndex);
-        console.log("End of numberA is " + endOfnumberAIndex);
+        console.log("Number A is " + numberA);
+        console.log("Number B is " + numberB);
+        console.log("Initial FinalCalcArray is " + finalCalcArray.join(''));
         let elementsToRemove = (endOfnumberBIndex - startOfnumberAIndex) + 1;
         let calculation = operations[operator](numberA, numberB);
+        console.log(`Result of Number A ${operator} Number B is ${calculation.toString().split('').join('')}`);
         let elementsToAdd = calculation.toString().split('');
         finalCalcArray.splice(startOfnumberAIndex, elementsToRemove, ...elementsToAdd);
-        console.log(calculation);
-        console.log(elementsToAdd);
-        console.log(finalCalcArray);
+        console.log("Resultant FinalCalcArray is " + finalCalcArray.join(''));
+        numberA = '';
+        numberB = '';
     }
 
-    while (finalCalcArray.find(operator => operations.includes(operator))) {
+    while (finalCalcArray.some(operator => operators.includes(operator))) {
         if (finalCalcArray.includes(operatorPrecedence.first)) {
             calculateTwoElements(operatorPrecedence.first);
             continue;
@@ -174,66 +157,13 @@ function calculate(finalCalcString) {
             break;
         }
     }
- /*       
-        
-        if (finalCalcArray[i] == operatorPrecedence.first) {
-            calculateTwoElements(operatorPrecedence.first);
-        } else if (finalCalcArray[i] == operatorPrecedence.second) {
-            calculateTwoElements(operatorPrecedence.second);
-        } else if (finalCalcArray[i] == operatorPrecedence.third) {
-            calculateTwoElements(operatorPrecedence.third);
-        } else if (finalCalcArray[i] == operatorPrecedence.fourth) {
-            calculateTwoElements(operatorPrecedence.fourth);
-        } else if (finalCalcArray[i] == operatorPrecedence.fifth) {
-            calculateTwoElements(operatorPrecedence.fifth);
-        } else {
-            i++;
-        }
-    }
-*/
-
-/*
-    let numberA = '';
-    let numberB = '';
-    let numberACompleted = false;
-    let numberBCompleted = false;
-    let operator = '';
-
-    function resetMathSection() {
-        numberA = '';
-        numberB = '';
-        numberACompleted = false;
-        numberBCompleted = false;
-        operator = '';
+    if (!isNaN(parseInt(finalCalcArray.join('')))) {
+        return finalCalcArray;
+    } else {
+        return "Incorrect Format";
     }
 
-
-    function calculateSection() {
-        for (let index = 0; index < finalCalcArray.length; index++) {
-            if (!(cfo(finalCalcArray[index])) && !(numberACompleted)) {
-                numberA += finalCalcArray[index];
-            } else if (!(cfo(finalCalcArray[index])) && !(numberBCompleted)) {
-                numberB += finalCalcArray[index];
-            } else if (cfo(finalCalcArray[index])) {
-                numberACompleted = true;
-                operator = finalCalcArray[index];
-            } else {
-                numberBCompleted = true;
-            }
-         }
-    }
-    
-    
-    calculateSection();
-    console.log(finalCalcArray)
-    runningCalculation = operations[operator](numberA, numberB);
-    resetMathSection();
-    
-    console.log(runningCalculation);
-*/
 }
-
-let runningCalculation = 0;
 
 function updateDisplay(event) {
     const button = event.target;
@@ -241,7 +171,7 @@ function updateDisplay(event) {
     if (button.value == 'AC' || button.value == 'CE') {
         displayElement.innerText = '';
     } else if (button.value == '=') {
-        calculate(displayElement.innerText);
+        displayElement.innerText = calculate(displayElement.innerText).join('');
     // If an operator has been pressed, then will add spacing to the display
     } else if (cfo(button.value, operators)) {      
         displayElement.innerText += ' ' + button.value;
